40f3f24a879da8815193cbd8f3af4473535114aa
Alexandre Lemarbre-Barrett <alexandre.lemarbre@gmail.com>
2024-01-13 15:42:53 -0500

    used float sqrt

--- world_info.py
+++ world_info.py
@@ -1,6 +1,5 @@
-from cmath import sqrt
+import math
 from game_message import Vector
 
-
 def get_euclidian_distance(s1: Vector, s2: Vector):
-    return sqrt(pow(s2.x - s1.x, 2) + pow(s2.y - s1.y, 2))
+    return math.sqrt(pow(s2.x - s1.x, 2) + pow(s2.y - s1.y, 2))
