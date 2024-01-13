e345c85e84a4e2789132947260380fe5e6f69f3b
Narvin Chana <narvin.chana@gmail.com>
2024-01-13 09:41:04 -0500

    feat: added csv export of ship vals

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from actions import *
 import random
 
 class Bot:
+    exportCSVData: bool = True
     target_team: str
 
     def __init__(self):
@@ -17,7 +18,7 @@ class Bot:
         return ship1.currentShield < ship2.currentShield
 
     # find ship if none targetted or current target doesn't exist anymore
-    def find_best_target(self, game_message: GameMessage): 
+    def find_best_target(self, game_message: GameMessage):
         if self.target_team != None:
             if game_message.ships.get(self.target_team) != None:
                 return
@@ -37,6 +38,31 @@ class Bot:
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
+
+        if self.exportCSVData:
+            f = open(f"info/export.csv", "w")
+            f.write("TurretType,MaxCharge,RocketBonusHullDamage,RocketBonusShieldDamage,RocketChargeCost,RocketDamage"
+                    ",RocketRadius,RocketSpeed,Rotatable\n")
+
+            turretInfos = game_message.constants.ship.stations.turretInfos
+            turrKeyList = list(turretInfos.keys())
+            for i in range(len(turretInfos.keys())):
+                f.write(
+                    f"{turrKeyList[i]},{turretInfos[turrKeyList[i]].maxCharge},{turretInfos[turrKeyList[i]].rocketBonusHullDamage},{turretInfos[turrKeyList[i]].rocketBonusShieldDamage},{turretInfos[turrKeyList[i]].rocketChargeCost},{turretInfos[turrKeyList[i]].rocketDamage},{turretInfos[turrKeyList[i]].rocketRadius},{turretInfos[turrKeyList[i]].rocketSpeed},{turretInfos[turrKeyList[i]].rotatable}\n")
+
+            f.write("ShieldBreakHandicap,ShieldRadius,ShieldRegenPercent\n")
+            shields = game_message.constants.ship.stations.shield
+            f.write(f"{shields.shieldBreakHandicap}, {shields.shieldRadius}, {shields.shieldRegenerationPercent}\n")
+
+            f.write("Radar Radius\n")
+            f.write(f"{game_message.constants.ship.stations.radar.radarRadius}\n")
+
+            f.write("Ship MaxHealth, MaxShield, MaxRotationDegs\n")
+            f.write(f"{game_message.constants.ship.maxHealth}, {game_message.constants.ship.maxShield},{game_message.constants.ship.maxRotationDegrees}\n")
+
+            f.close()
+            self.exportCSVData = False
+
         actions = []
 
         self.find_best_target(game_message)
@@ -45,7 +71,7 @@ class Bot:
         my_ship = game_message.ships.get(team_id)
         other_ships_ids = [shipId for shipId in game_message.shipsPositions.keys() if shipId != team_id]
 
-        
+
 
         # Find who's not doing anything and try to give them a job?
         idle_crewmates = [crewmate for crewmate in my_ship.crew if crewmate.currentStation is None and crewmate.destination is None]
